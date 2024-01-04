2860df466ceef8ed16c77d338449243880c0b275
Tyrone Joel <tyronejoel04@gmail.com>
2024-01-04 13:20:20 -0800

    File update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,37 +1,17 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application """
+"""Start a flask web app
+"""
+
 from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """ Prints a Message when / is called """
+@app.route('/')
+def hello_flask():
+    """Return string when route queried
+    """
     return 'Hello HBNB!'
 
-if __name__ == "__main__":
-    """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
-
-web_flask/1-hbnb_route.py
-
-#!/usr/bin/python3
-""" Starts a Flash Web Application HBNB"""
-from flask import Flask
-app = Flask(__name__)
-
-
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """ Prints a Message when / is called """
-    return 'Hello HBNB!'
-
-
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """ Prints a Message when /hbnb is called """
-    return 'HBNB'
-
-if __name__ == "__main__":
-    """ Main Function """
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
